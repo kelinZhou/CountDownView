a31f8b485a7418fef2d9e79c0caab38a7d500026
kelin <kelin.zhouql@lieluobo.com>
2018-01-25 10:41:02 +0800

    update demo
    。

--- app/src/main/java/com/kelin/countdownviewdemo/MainActivity.java
+++ app/src/main/java/com/kelin/countdownviewdemo/MainActivity.java
@@ -2,27 +2,32 @@ package com.kelin.countdownviewdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 import com.kelin.countdownview.CountDownView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CountDownView cdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((CountDownView)findViewById(R.id.cd_view)).setOnFinishListener(new CountDownView.OnFinishListener() {
+        cdView = findViewById(R.id.cd_view);
+        cdView.setOnFinishListener(new CountDownView.OnFinishListener() {
             @Override
             public void onFinish() {
                 Toast.makeText(getApplicationContext(), "倒计时完毕！", Toast.LENGTH_SHORT).show();
             }
-        }).start();
+        });
+        cdView.start();
     }
 
-//    public void onStartCountDown(View view) {
-//        if (!cdView.isStarted()) {
-//            cdView.start();
-//        }
-//    }
+    public void onStartCountDown(View view) {
+        if (!cdView.isStarted()) {
+            cdView.start();
+        }
+    }
 }
